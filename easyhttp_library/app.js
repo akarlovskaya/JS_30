48758c889bb836e37ace121e491c96604e576c1d
Anna <karlovskaya@gmail.com>
2018-07-02 14:04:05 -0700

    add Post, Put, Delete methods

--- easyhttp_library/app.js
+++ easyhttp_library/app.js
@@ -1,12 +1,46 @@
 var myHttp = new EasyHTTP();
-
+// GET One post from server
 myHttp.get('https://jsonplaceholder.typicode.com/posts/1',
-            function(error, response){
+                function(error, response){
+                    if (error) {
+                        console.error(error);
+                    } else {
+                        console.log(response);
+                    }
+                }
+            );
+// Create data
+var data = {
+    'title': 'My Title',
+    'body': 'My post body'
+};
+// POST a new post
+myHttp.post('https://jsonplaceholder.typicode.com/posts', data, function(error, post){
+                if (error) {
+                    console.error(error);
+                } else {
+                    console.log(post);
+                }
+            });
+
+// PUT (update) a post
+var updatedData = {
+    'title': 'My New Title',
+    'body': 'My new post body'
+};
+myHttp.put('https://jsonplaceholder.typicode.com/posts/1', updatedData, function(error, response){
                 if (error) {
                     console.error(error);
                 } else {
                     console.log(response);
                 }
+            });
 
-            }
-        );
+// DELETE a post
+myHttp.delete('https://jsonplaceholder.typicode.com/posts/1', function(error, response){
+                if (error) {
+                    console.error(error);
+                } else {
+                    console.log(response);
+                }
+            });
--- easyhttp_library/easyhttp.js
+++ easyhttp_library/easyhttp.js
@@ -1,12 +1,10 @@
-/*====
-Make an HTTP GET Request
-=====*/
 // create function constructor
 function EasyHTTP() {
     this.http = new XMLHttpRequest();
 }
+// add methods to prototype passing a user params
 
-// add get method to prototype with passing a user url param
+// Make an HTTP GET Request
 EasyHTTP.prototype.get = function (url, callback) {
     this.http.open('GET', url, true);
     // define "self" variable to get it from .onload function scope, overwise getting "undefined"
@@ -24,7 +22,39 @@ EasyHTTP.prototype.get = function (url, callback) {
 };
 
 // Make an HTTP POST Request
+EasyHTTP.prototype.post = function (url, data, callback) {
+    // initialize a new request to post data
+    this.http.open('POST', url, true);
+    // Set content type in Http Header
+    this.http.setRequestHeader('Content-type', 'application/json');
+
+    var self = this;
+    this.http.onload = function() {
+        // get new post or error
+        callback(null, self.http.responseText);
+    };
+    // send the request as string to server
+    this.http.send(JSON.stringify(data));
+};
 
 // Make an HTTP PUT Request
+EasyHTTP.prototype.put = function(url, data, callback){
+    this.http.open('PUT', url, true);
+    this.http.setRequestHeader('Content-type', 'application/json');
+
+    var self = this;
+    this.http.onload = function(){
+        callback(null, self.http.responseText);
+    };
+    this.http.send(JSON.stringify(data));
+};
 
 // Make an HTTP DELETE Request
+EasyHTTP.prototype.delete = function(url, callback){
+    this.http.open('DELETE', url, true);
+    this.http.onload = function(){
+        callback(null, 'Post deleted');
+    };
+
+    this.http.send();
+};

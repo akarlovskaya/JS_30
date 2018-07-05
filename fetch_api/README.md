839fd2a3e5bf92fef90b87b9814439ff854cf4ac
Anna <karlovskaya@gmail.com>
2018-07-05 10:39:54 -0700

    add generic errorHandler function, update comments

--- fetch_api/README.md
+++ fetch_api/README.md
@@ -1,4 +1,5 @@
 # Fetch API Practice
 
 ## Features
-1. ES6 syntax
+1. ES5 and ES6 syntax Practice
+2. Practice fetch() API to get local txt file, local JSON and external API (https://api.github.com/users)
--- fetch_api/app.js
+++ fetch_api/app.js
@@ -11,33 +11,27 @@ BTN_1.addEventListener('click', getText);
 BTN_2.addEventListener('click', getJson);
 BTN_3.addEventListener('click', getExternal);
 
-// Get and output data from text file
+// Get and output data from TEXT file
 function getText() {
     // call fetch() method with 'test.txt; file as parameter to get it
-    fetch('test.txt') // fetch responses Promise object
-        // call then() method to get data
-        .then(
-            // Transform the data into text
-            function(response) {
-                return response.text();
-            })
-        // get data
-        .then(function(data){
+    fetch('test.txt') // fetch responses with Promise object
+        .then(errorHandler) // error handling (like 404/500)
+        .then(response => response.text()) // Transform the data into text
+        .then((data) => { // when get data, output it
             TEXT_OUTPUT_EL.innerHTML = data;
             JSON_OUTPUT_EL.innerHTML = '';
             APIDATA_OUTPUT_EL.innerHTML = '';
         })
-        .catch(function(err){
-            console.log(err);
-        });
-}
-// Get local Json from localhost
+        //fetch() will only reject a promise if the user is offline, or some unlikely networking error occurs, such a DNS lookup failure.
+        .catch(err => console.error(err)); // Fetch rejects if it's unable to give a response due to an error, but a 500/404 response isn't that.
+    }
+// Get local JSON from localhost
 function getJson(){
     fetch('posts.json') // tell what to fetch
         .then(function(response){ // here we get Promise object <pending> as response
-            return (response.json()); // transform response to json and
+            return response.json(); // transform response to json
         })
-        .then(function(data){ // here we get data (Array)
+        .then(data => { // here we get data (Array)
             let output = '';
 
             data.forEach(function(post){
@@ -47,18 +41,14 @@ function getJson(){
             TEXT_OUTPUT_EL.innerHTML = '';
             APIDATA_OUTPUT_EL.innerHTML = '';
         })
-        .catch(function(err){
-            console.log('Error: ', err);
-        });
+        .catch(err => console.error(err));
 }
 
 // Get GIT users from GIT API
 function getExternal(){
     fetch('https://api.github.com/users')
-        .then(response => {
-            return response.json();
-        })
-        .then(function(data){
+        .then(response => response.json())
+        .then(data => {
             let output = '';
 
             data.forEach(function(user, index){
@@ -68,7 +58,13 @@ function getExternal(){
             TEXT_OUTPUT_EL.innerHTML = '';
             JSON_OUTPUT_EL.innerHTML = '';
         })
-        .catch(function(err){
-            console.error('Error: ', err);
-        });
+        .catch(err => console.error(err));
+}
+
+// Generic Error handling function
+function errorHandler(response){
+    if ( !response.ok ) {
+        throw Error(response.statusText);
+    }
+    return response;
 }
